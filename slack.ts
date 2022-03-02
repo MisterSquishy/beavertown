2633d0076816a8ed69d7a475a5da289301d7760b
MisterSquishy <pdavids@comcast.net>
2022-03-01 19:32:02 -0500

    run on 3k

--- slack.ts
+++ slack.ts
@@ -5,7 +5,8 @@ import { ChatPostMessageArguments, WebClient } from "@slack/web-api";
 const client = new WebClient(process.env.SLACK_BOT_TOKEN);
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  port: 3000,
 });
 
 const hype = [
